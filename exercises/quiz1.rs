a2c91687cdc611e0a00c1915d8bc52165563712f
Mikola <60260972+hiisync@users.noreply.github.com>
2024-01-26 20:55:13 +0200

    Quiz 1 is completed

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(count: i32) -> i32 {
+    let mut price = 2;
+    if count > 40 {
+        price = 1;
+    }
+
+    let sum = count * price;
+
+    sum
+}
 
 // Don't modify this function!
 #[test]
